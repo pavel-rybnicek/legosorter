5853a9228927bf6f9600bf2516d6737f9995993c
pavel-rybnicek <Pavel.Rybnicek@email.cz>
2017-06-10 23:47:33 +0200

    / Pokus o zmereni rychlosti snimkovani.

--- legosorter_picam/src/pi/picam/PicamServer.java
+++ legosorter_picam/src/pi/picam/PicamServer.java
@@ -1,8 +1,13 @@
 package pi.picam;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.hopding.jrpicam.RPiCamera;
+import com.hopding.jrpicam.enums.Exposure;
 import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 
 public class PicamServer {
@@ -12,6 +17,28 @@ public class PicamServer {
 		// Create a Camera that saves images to the Pi's Pictures directory.
 		RPiCamera piCamera = new RPiCamera("/home/pi/Pictures");
 		piCamera.takeStill("An Awesome Pic.jpg");	
+		
+		piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
+	    .setBrightness(75)                // Adjust Camera's brightness setting.
+	    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
+	    .setTimeout(2)                    // Set Camera's timeout.
+	    .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
+	// Sets all Camera options to their default settings, overriding any changes previously made.
+	piCamera.setToDefaults();
+	
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	
+	Date date = new Date();
+	System.out.println(dateFormat.format(date));
+	BufferedImage image = piCamera.takeBufferedStill();
+	date = new Date();
+	System.out.println(dateFormat.format(date));
+	 piCamera.takeBufferedStill();
+	date = new Date();
+	System.out.println(dateFormat.format(date));
+	 piCamera.takeBufferedStill();
+	date = new Date();
+	System.out.println(dateFormat.format(date));
 	}
 
 }
