d747f1c774c5b77ea6eccb444ecb51adffde4cec
pavel-rybnicek <Pavel.Rybnicek@email.cz>
2017-06-10 21:30:03 +0200

    + Prvni pokus s kamerou.

--- legosorter_picam/src/pi/picam/PicamServer.java
+++ legosorter_picam/src/pi/picam/PicamServer.java
@@ -1,11 +1,17 @@
 package pi.picam;
 
+import java.io.IOException;
+
+import com.hopding.jrpicam.RPiCamera;
+import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
+
 public class PicamServer {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FailedToRunRaspistillException, IOException, InterruptedException 
 	{
-		System.out.println("Hello world");
-
+		// Create a Camera that saves images to the Pi's Pictures directory.
+		RPiCamera piCamera = new RPiCamera("/home/pi/Pictures");
+		piCamera.takeStill("An Awesome Pic.jpg");	
 	}
 
 }
