65bb63f2b27fdf652c20cadad573644a84e7aafa
pavel-rybnicek <Pavel.Rybnicek@email.cz>
2017-06-17 15:08:54 +0000

    Pridana automaticka korekce jasu - reakce na vybijejici se osvetleni.

--- picam_server/picam_server.py
+++ picam_server/picam_server.py
@@ -9,9 +9,13 @@ import time
 import struct
 from thread import *
 from picamera import PiCamera
+import numpy as np
  
 HOST = '0.0.0.0'   # Symbolic name meaning all available interfaces
 PORT = 8001 # Arbitrary non-privileged port
+
+MIN_BRT = 160
+MAX_BRT = 170
  
 camera = PiCamera(resolution=(640, 480), framerate=30)
 # Set ISO to the desired value
@@ -25,6 +29,7 @@ g = camera.awb_gains
 camera.awb_mode = 'auto'
 camera.awb_gains = g
 camera.brightness = 55
+camera.contrast = 55
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -47,14 +52,20 @@ print 'Socket now listening'
 def clientthread(conn):
     #Sending message to connected client
 #    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
-     
+    
     my_stream = io.BytesIO()
     #infinite loop so that function do not terminate and thread do not end.
+    i = 0
     while True:
          
         #Receiving from client
         print '1 ',time.time()
         data = conn.recv(1024)
+
+        # zkorigujeme jas
+        if i > 40:
+            adjustBrightness()
+            i = 0
         
         camera.capture(my_stream, 'jpeg', use_video_port = True)
        
@@ -72,10 +83,42 @@ def clientthread(conn):
         # Reset the stream for the next capture
         my_stream.seek(0)
         my_stream.truncate()
+
+        i = i + 1
         
  
     #came out of loop
     conn.close()
+ 
+# srovnani jasu na konstantni uroven
+def setBrightness():
+    while getBrightness() < MIN_BRT:
+        camera.brightness = camera.brightness + 1
+    
+    while getBrightness() > MAX_BRT:
+        camera.brightness = camera.brightness - 1
+
+# srovnani jasu na konstantni uroven
+def adjustBrightness():
+    currentBrt = getBrightness()
+    
+    if currentBrt < MIN_BRT:
+        camera.brightness = camera.brightness + 1
+    elif currentBrt > MAX_BRT:
+        camera.brightness = camera.brightness - 1
+
+# zjisti aktualni jas bile plochy
+def getBrightness():
+    output = np.empty((640 * 480 * 3,), dtype=np.uint8)
+    camera.capture(output, 'rgb')
+    output = output.reshape((640, 480, 3))
+    output = output[70:570, :110, :] 
+    currentBrt = np.average(output)
+    print currentBrt
+    return currentBrt
+
+# nastavime inicialni jas
+setBrightness()
  
 #now keep talking with the client
 while 1:
